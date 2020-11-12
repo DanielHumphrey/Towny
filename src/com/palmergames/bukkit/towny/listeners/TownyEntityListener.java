5dfd308c415d74b010f1fda4bec25507f05daa86
Llm Dl <LlmDlio@gmail.com>
2020-11-12 07:59:10 -0600

    TownyExplodeEvent commit.
    - Adds TownyExplosionEvent which tests only for a location.
      - Currently only used for Wither explosions.
    - Adds javadoc comments explaining when the new explosion events are
    being called.

--- src/com/palmergames/bukkit/towny/listeners/TownyEntityListener.java
+++ src/com/palmergames/bukkit/towny/listeners/TownyEntityListener.java
@@ -551,7 +551,7 @@ public class TownyEntityListener implements Listener {
 
 		case WITHER:
 
-			if (!TownyActionEventExecutor.isAllowedExplosion(event.getBlock().getLocation())) {
+			if (!TownyActionEventExecutor.canLocationExplode(event.getBlock().getLocation())) {
 				event.setCancelled(true);
 				return;
 			}
